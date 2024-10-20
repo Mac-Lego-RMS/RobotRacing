551a788ae53136a1ba776d5b0b6fe4b74f2eb720
Florian H <Florian.Hoos05@gmail.com>
2024-10-20 18:24:57 +0200

    Create RobotRacing.py
    Grobes Layout des Programms
    Grundstruktur anhand von Kommentaren

---
+++ RobotRacing.py
@@ -0,0 +1,41 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch
+
+################################################################################
+# Settings
+################################################################################
+
+# Steering Settings - Car-Steering or Tank-Steering
+IsCar = False
+
+
+
+################################################################################
+# Declarations
+################################################################################
+hub = PrimeHub()
+
+
+
+
+################################################################################
+# Functions
+################################################################################
+
+
+
+
+
+################################################################################
+# Main Loop
+################################################################################
+while True:
+    # Get the Speed and Steering inputs from the user
+    
+    # Do Calculations with the values
+
+    # pass the Values to the Motor Control
+    
