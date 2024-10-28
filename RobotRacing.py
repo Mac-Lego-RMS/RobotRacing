249c98dcc9b98f2d0af0eab474b493eb279f673a
Florian H <Florian.Hoos05@gmail.com>
2024-10-28 22:32:57 +0100

    Enabled Car-Thing and enhanced Boost-Function

--- RobotRacing.py
+++ RobotRacing.py
@@ -4,6 +4,7 @@ from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
 from pybricks.robotics import DriveBase, Car
 from pybricks.tools import wait, StopWatch
 from pybricks.iodevices import XboxController 
+from pybricks.geometry import Matrix
 
 
 '''
@@ -13,23 +14,77 @@ from pybricks.iodevices import XboxController
 3. Spike sollte sich jetzt automatisch verbinden und das Programm starten
 Danach verbindet sich der XBOX-Controller automatisch bei Programmstart
 '''
+################################################################################
+# restliche Deklarationen
+################################################################################
+
+h = 100 # helligkeit der Matrize
+Bluetooth_icon = Matrix(
+    [
+    [h,h,h,0,0],
+    [h,0,0,h,0],
+    [h,h,h,0,0],
+    [h,0,0,h,0],
+    [h,h,h,0,0]
+    ]
+)
+Go_icon = Matrix(
+    [
+    [0,0,h,0,0],
+    [0,h,0,h,0],
+    [h,0,h,0,h],
+    [0,h,0,h,0],
+    [h,0,0,0,h]
+    ]
+)
+Boost_icon = Matrix(
+    [
+    [0,0,h,0,0],
+    [0,h,h,h,0],
+    [h,0,h,0,h],
+    [0,0,h,0,0],
+    [0,h,h,h,0]
+    ]
+)
+hub = PrimeHub()
+
+hub.speaker.volume(70)
+hub.speaker.beep(300,150)
+hub.speaker.beep(400,150)
+hub.speaker.beep(500,150)
+
+hub.display.icon(Bluetooth_icon)
+
+MyController = XboxController()
+
+hub.display.icon(Go_icon)
+hub.speaker.beep(600,150)
+hub.speaker.beep(700,150)
+
+MyController.rumble(
+    power=50,
+    duration=200,
+    count=2,
+    delay=50
+)
 
 ################################################################################
-# Einstellungen
+# Steuerung
 ################################################################################
+
 
 # Steuerungseinstellungen
 VideoGameSteering = True
 
 # Roboter-Aufbau - Car-Steering oder Tank-Steering
-IsTank = True
+IsTank = False
+MehrereAntriebsMotoren = True
 
 # Geschwindigkeits-Einstellungen
 Max_Speed = 1000 # in °/s 
 CapNorm = 0.5 # used for Boost-Function
 BoostmaxTime = 2 # in Sec, states how long the boost is available for
 BoostCooldownTime = 6 # in Sec, Cooldown time for boost function
-
 
 
 if IsTank:
@@ -47,30 +102,28 @@ if IsTank:
     )
 
 else:
+    print("Car")
+    SteeringMotor = Motor(Port.A)
+
+    if MehrereAntriebsMotoren:
+        Antrieb = [
+            Motor(Port.B,Direction.COUNTERCLOCKWISE),
+            Motor(Port.C)
+        ]
+    else
+        Motor(Port.B,Direction.COUNTERCLOCKWISE)
+
+    
     car = Car(
-        steer_motor=Motor(Port.A),
-        drive_motors=Motor(Port.B),
+        steer_motor=SteeringMotor,
+        drive_motors=Antrieb,
         torque_limit=100
     )
     car.drive_speed(0)
+    car.drive_power(0)
 
-################################################################################
-# restliche Deklarationen
-################################################################################
-hub = PrimeHub()
 
-hub.speaker.volume(80)
-hub.speaker.beep(400,100)
-MyController = XboxController()
-hub.speaker.beep(500,100)
-MyController.rumble(
-    power=50,
-    duration=200,
-    count=2,
-    delay=50
-)
-BoostActive=False
-BoostSW = StopWatch()
+
 ################################################################################
 # Funktionen
 ################################################################################
@@ -113,9 +166,19 @@ def translate(value, leftMin, leftMax, rightMin, rightMax):
     # Convert the 0-1 range into a value in the right range.
     return rightMin + (valueScaled * rightSpan)
 
+
+
+
+
 ################################################################################
 # Main Loop
 ################################################################################
+
+
+BoostActive=False
+boostready = False
+BoostSW = StopWatch()
+
 while True:
     # Auslesen XBOX-Controller - Erfassen von Geschwindigkeit & Lenken
     Pressed = MyController.buttons.pressed()
@@ -132,12 +195,26 @@ while True:
         print("started boost")
         hub.speaker.beep(900,200)
         hub.light.blink(color=Color.CYAN,durations=[150,100])
+        MyController.rumble(
+            power=50,
+            duration=50,
+            count=1,
+            delay=50
+        )
 
     if not BoostActive:
         MotorSpeed = MotorSpeed * CapNorm
-        hub.light.on(color=Color.YELLOW)
         if (BoostSW.time() > BoostCooldownTime*1000):
             hub.light.blink(color=Color.GREEN,durations=[200,500])
+            hub.display.icon(Boost_icon)
+            if not boostready:
+                MyController.rumble(
+                    power=100,
+                    duration=75,
+                    count=1,
+                    delay=50
+                )
+            boostready = True
     else:
         print(BoostSW.time())
         if BoostSW.time() < BoostmaxTime*1000:
@@ -145,7 +222,10 @@ while True:
         else:
             BoostActive = False
             print("boost over")
+            hub.light.on(color=Color.YELLOW)
             hub.speaker.beep(800,100)
+            hub.display.icon(Go_icon)
+            boostready = False
 
     MotorSpeed = int(MotorSpeed)
 
@@ -153,9 +233,14 @@ while True:
     MotorSteer = MySteering[1] * -1 # -100 bis 100
     if abs(MotorSteer) <20:
         MotorSteer=0
+        if not IsTank:
+            SteeringMotor.stop
     else:
       MotorSteer = int(translate(MotorSteer,-100,100,-300,300))
 
+    if Button.GUIDE in Pressed:
+        raise SystemExit
+        # Ausschalten über XBOX-Button
 
     #print("Speed",format(MotorSpeed,000),"Steering:",MotorSteer)
 
