2e211eff86fcba20666d8d846aab1923b5928d0b
Florian Hoos <Florian.Hoos@gmail.com>
2024-11-04 14:11:07 +0100

    clean up, not tested!
    This has not yet been tested with a Spike Prime Robot.

--- RobotRacing.py
+++ RobotRacing.py
@@ -62,55 +62,67 @@ hub.speaker.beep(600,150)
 hub.speaker.beep(700,150)
 
 MyController.rumble(
-    power=50,
-    duration=200,
-    count=2,
-    delay=50
+    power    = 50,
+    duration = 200,
+    count    = 2,
+    delay    = 50
 )
+
+################################################################################
+# Globale Variable, die von mehreren Funktionen aufgerufen werden:
+################################################################################
+BoostActive = False
+boostready  = False
+BoostSW     = StopWatch()
+
 
 ################################################################################
 # Steuerung
 ################################################################################
 
 
-# Steuerungseinstellungen
-VideoGameSteering = True
-
 # Roboter-Aufbau - Car-Steering oder Tank-Steering
 IsTank = False
 MehrereAntriebsMotoren = True
 
 # Geschwindigkeits-Einstellungen
-Max_Speed = 1000 # in °/s 
-CapNorm = 0.5 # used for Boost-Function
-BoostmaxTime = 2 # in Sec, states how long the boost is available for
-BoostCooldownTime = 6 # in Sec, Cooldown time for boost function
+Max_Speed = 100         # in °/s  Maximum Speed
+CapNorm = 0.5           # in %    used for Boost-Function
+BoostmaxTime = 2        # in Sec  states how long the boost is available for
+BoostCooldownTime = 6   # in Sec  Cooldown time for boost function
 
 
 if IsTank:
+    print("Tank")
     drive_base = DriveBase(
         left_motor  = Motor(Port.A),
         right_motor = Motor(Port.B, Direction.COUNTERCLOCKWISE),
-        wheel_diameter=56,
-        axle_track=96
+        wheel_diameter= 56,
+        axle_track = 96
     )
     drive_base.settings(
-        straight_speed=900,
-        straight_acceleration=1000,
-        turn_rate=500,
-        turn_acceleration=1000
+        straight_speed = 900,
+        straight_acceleration = 1000,
+        turn_rate = 500,
+        turn_acceleration = 1000
     )
 
 else:
     print("Car")
     SteeringMotor = Motor(Port.A,Direction.COUNTERCLOCKWISE)
-    Antrieb = Motor(Port.B,Direction.COUNTERCLOCKWISE)
-
     
+    if MehrereAntriebsMotoren:
+        Antrieb = [
+            Motor(Port.B,Direction.COUNTERCLOCKWISE),
+            Motor(Port.C)
+        ]
+    else
+        Antrieb = Motor(Port.B,Direction.COUNTERCLOCKWISE)
+
     car = Car(
-        steer_motor=SteeringMotor,
-        drive_motors=Antrieb,
-        torque_limit=50
+        steer_motor  = SteeringMotor,
+        drive_motors = Antrieb,
+        torque_limit = 30
     )
     car.drive_speed(0)
     car.drive_power(0)
@@ -134,23 +146,9 @@ def GetSteeringValues(): # Returns a Tupel of Speed and Steering
     # Steering Value from -100 to 100
     return CurrSpeed, CurrSteering
 
-
-def MotorControll(MySpeed,MySteer):
-    if IsTank:    
-        drive_base.drive(
-            speed=MySpeed,
-            turn_rate=MySteer
-        )
-    else:
-        car.drive_speed(
-            speed=MySpeed
-        )
-        car.steer(
-            percentage=MySteer
-        )
-def translate(value, leftMin, leftMax, rightMin, rightMax):
+def translate(value, leftMin, leftMax, rightMin, rightMax): # "map"-function
     # Figure out how 'wide' each range is
-    leftSpan = leftMax - leftMin
+    leftSpan  = leftMax  - leftMin
     rightSpan = rightMax - rightMin
 
     # Convert the left range into a 0-1 range (float)
@@ -159,28 +157,8 @@ def translate(value, leftMin, leftMax, rightMin, rightMax):
     # Convert the 0-1 range into a value in the right range.
     return rightMin + (valueScaled * rightSpan)
 
-
-
-
-
-################################################################################
-# Main Loop
-################################################################################
-
-
-BoostActive=False
-boostready = False
-BoostSW = StopWatch()
-
-while True:
-    # Auslesen XBOX-Controller - Erfassen von Geschwindigkeit & Lenken
-    Pressed = MyController.buttons.pressed()
-    MySteering = GetSteeringValues() # Returns a Tupel
-
-    # Verarbeiten der Werte
-
-    MotorSpeed = MySteering[0] * 10 # -1000 bis 1000
-
+def Calculate_Boost(SpeedIN):
+    global Pressed, boostready, BoostSW, BoostCooldownTime #needs to be checked
 
     if (not BoostActive) and (Button.LB in Pressed) and (BoostSW.time() > BoostCooldownTime*1000):
         BoostSW.reset()
@@ -196,7 +174,7 @@ while True:
         )
 
     if not BoostActive:
-        MotorSpeed = MotorSpeed * CapNorm
+        SpeedIN = SpeedIN * CapNorm
         if (BoostSW.time() > BoostCooldownTime*1000):
             hub.light.blink(color=Color.GREEN,durations=[200,500])
             hub.display.icon(Boost_icon)
@@ -220,25 +198,56 @@ while True:
             hub.display.icon(Go_icon)
             boostready = False
 
-    MotorSpeed = int(MotorSpeed)
+    return int(MotorSpeed)
+    
+
+def MotorControl(MySpeed,MySteer):
+    if IsTank:    
+        drive_base.drive(
+            speed = MySpeed, 
+            turn_rate = MySteer
+        )
+    else:
+        car.drive_power(
+            power = MySpeed # Number in %
+        )
+        car.steer(
+            percentage = MySteer
+        )
 
 
-    MotorSteer = MySteering[1] * -1 # -100 bis 100
-    if abs(MotorSteer) <20:
+################################################################################
+# Main Loop
+################################################################################
+
+while True:
+    # Auslesen XBOX-Controller - Erfassen von Geschwindigkeit & Lenken
+    Pressed = MyController.buttons.pressed()
+    Control_Values = GetSteeringValues() # Returns a Tupel of Speed and Steering
+
+    # Verarbeiten der Werte
+    MotorSpeed = Control_Values[0] * 10 # -1000 bis 1000
+    MotorSteer = Control_Values[1] * -1 # -100% bis 100%
+
+    # Boost verarbeiten und Geschwindigkeit anpassen
+    MotorSpeed = Calculate_Boost(MotorSpeed)
+
+    # Wenn zu wenig gelenkt wird, soll der Lenk-Motor ausgeschaltet werden.
+    if abs(MotorSteer) < 10:
         MotorSteer=0
         if not IsTank:
             SteeringMotor.stop
     else:
-      MotorSteer = int(translate(MotorSteer,-100,100,-300,300))
+        MotorSteer = int(translate(MotorSteer,-100,100,-300,300))
 
-    if Button.GUIDE in Pressed:
-        raise SystemExit
-        # Ausschalten über XBOX-Button
-
-    #print("Speed",format(MotorSpeed,000),"Steering:",MotorSteer)
-
-    print("Speed:","{: 05d}".format(MotorSpeed),"Steering","{: 04d}".format(MotorSteer),"   ",BoostSW.time())
+    print("Speed:","{: 05d}".format(MotorSpeed),"Steering:","{: 04d}".format(MotorSteer),"   ",BoostSW.time())
+    
     # Übergabe der Werte an die Motor-Steuerung
-    MotorControll(MotorSpeed,MotorSteer)
+    MotorControl(MotorSpeed,MotorSteer)
+
+    # Ausschalten über XBOX-Button
+    if Button.GUIDE in Pressed:
+        XboxController = None # Entdeklarieren des XBOX-Controllers
+        raise SystemExit
 
     wait(10)
